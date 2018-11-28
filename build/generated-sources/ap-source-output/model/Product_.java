e5f2a06352b996daaa15b49c2fbc2d9b032e2b6d
Farmez <thinnapat.2542@mail.kmutt.ac.th>
2018-11-28 23:54:46 +0700

    [UPDATE] *

--- build/generated-sources/ap-source-output/model/Product_.java
+++ build/generated-sources/ap-source-output/model/Product_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-11-28T23:34:47")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-11-28T23:46:58")
 @StaticMetamodel(Product.class)
 public class Product_ { 
 
